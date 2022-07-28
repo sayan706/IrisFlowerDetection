8df35212c901931cbe3cdd33304613d4b3bd4173
sayan706 <sayanchatterjee706@gmail.com>
2022-07-28 19:10:06 +0530

    updated by sayan and supratim

--- iris/urls.py
+++ iris/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('home/',views.index ),
+    path('',views.index ),
     path('predict/',views.predict)
 
 ]
